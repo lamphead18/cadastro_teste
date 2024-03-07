a8ad3e1ef4a95e21ed2c5c196559be9fc8f41814
gabrielfpigozzo@gmail.com <gabrielfpigozzo@gmail.com>
2024-03-07 16:09:23 -0300

    atualizações

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../../components/Button";
+import Input from "../../components/Input";
 import * as C from './styles';
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
@@ -11,10 +12,48 @@ const Home = () => {
 
     return (
         <C.Container>
-            <C.Title>Início</C.Title>
-            <Button Text="Sair" onClick={() => [signout(), navigate("/")]} >
-                Sair
-            </Button>
+            <C.Title>Inserir Logomarca e Certificado</C.Title>
+            <C.Label>CNPJ</C.Label>
+            <C.Content>
+                <Input type="text" placeholder="Digite o CNPJ" />
+
+                <C.Label>Nome Fantasia</C.Label>
+                <Input type="text" placeholder="Digite o nome fantasia" />
+
+                <C.Label>Contato</C.Label>
+                <Input type="text" placeholder="Digite o contato" />
+
+                <C.Label>Endereço</C.Label>
+                <Input type="text" placeholder="Digite o endereço" />
+
+                <C.Label>Número</C.Label>
+                <Input type="text" placeholder="Digite o número" />
+
+                <C.Label>Bairro</C.Label>
+                <Input type="text" placeholder="Digite o bairro" />
+
+                <C.Label>Cidade</C.Label>
+                <Input type="text" placeholder="Digite a cidade" />
+
+                <C.Label>UF</C.Label>
+                <Input type="text" placeholder="Digite a UF" />
+
+                <C.Label>WhatsApp</C.Label>
+                <Input type="text" placeholder="Digite o WhatsApp" />
+
+                <C.Label>E-mail</C.Label>
+                <Input type="text" placeholder="Digite o e-mail" />
+
+                <C.Label>Status</C.Label>
+                <select>
+                    <option value="ativo">Ativo</option>
+                    <option value="inativo">Inativo</option>
+                </select>
+
+                <Button Text="Sair" onClick={() => [signout(), navigate("/")]} >
+                    Sair
+                </Button>
+            </C.Content>
         </C.Container>
     )
 }
--- src/pages/Home/styles.jsx
+++ src/pages/Home/styles.jsx
@@ -12,3 +12,23 @@ export const Container = styled.div`
 export const Title = styled.h2`
     
 `;
+
+export const Content = styled.div`
+    gap: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    box-shadow: 0 1px 2px #0003;
+    background-color: white;
+    max-width: 350px;
+    padding: 20px;
+    border-radius: 5px;
+`;
+
+export const Label = styled.label`
+    font-size: 18px;
+    font-weight: 600;
+    color: #676767;
+`;
